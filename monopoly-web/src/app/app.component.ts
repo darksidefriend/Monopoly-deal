bbb24924670ee21e326bfa644e376c600399783e
darksidefriend <dansavels@yandex.ru>
2023-03-21 19:00:16 +0300

    added smthng

--- monopoly-web/src/app/app.component.ts
+++ monopoly-web/src/app/app.component.ts
@@ -18,9 +18,9 @@ export class AppComponent implements OnInit{
 
   @ViewChildren(PlayerFieldComponent)allPlayerComponents! : QueryList<PlayerFieldComponent> ;
   
-  gameIsReady : boolean = false;
-  gameIsActive : boolean = false;
-  playerList : string[] = [];
+  gameIsReady : boolean = true;
+  gameIsActive : boolean = true;
+  playerList : string[] = ['dick', 'suck', 'pussy'];
   activePlayerID = 0;
   activePlayer : string = this.playerList[this.activePlayerID];
   cardDeck = Deck.cardDeck;
@@ -62,6 +62,10 @@ export class AppComponent implements OnInit{
     
   }
 
+  loadOpponentsList(playerName : string){        
+    return this.playerList.filter(elem => elem !== playerName);
+  }
+
   changePlayer(){ 
     this.activePlayerID = (this.activePlayerID + 1) % this.playerList.length;
     this.activePlayer = this.playerList[this.activePlayerID];
@@ -81,41 +85,318 @@ export class AppComponent implements OnInit{
       case 'rent':
 
         if (cardSelected.color === 'any'){
-          this.allPlayerComponents.toArray().find((elem) => {elem.playerName === this.activePlayer})!.playerOpenCards;
-          console.log(this.allPlayerComponents.toArray().find((elem) => {elem.playerName === this.activePlayer})!.playerOpenCards);
-          availableCards.push({playerName : this.activePlayer, playerCards : this.allPlayerComponents.toArray().find((elem) => {elem.playerName === this.activePlayer})!.playerOpenCards});
+          if (this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerOpenCards.length == 0){
+            alert('У вас нет карт собственности!')
+            return;
+          }
+          this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardSelectorMode = true;
+          availableCards.push({playerName : this.activePlayer, playerCards : this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerOpenCards});
         } else {
-          let availableColors = cardSelected.color!.split('/');
-          this.allPlayerComponents.toArray().find((elem) => {elem.playerName === this.activePlayer})!.playerOpenCards.filter(card => (card.color === availableColors[0] || card.color === availableColors[1]));
-          console.log(this.allPlayerComponents.toArray().find((elem) => {elem.playerName === this.activePlayer})!.playerOpenCards.filter(card => (card.color === availableColors[0] || card.color === availableColors[1])));
-          availableCards.push({playerName : this.activePlayer, playerCards : this.allPlayerComponents.toArray().find((elem) => {elem.playerName === this.activePlayer})!.playerOpenCards});
+          let availableColors = cardSelected.color;
+          if (this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerOpenCards.filter(card => (card.color === availableColors)).length == 0){
+            alert('У вас нет карт собственности такого цвета!')
+            return;
+          }
+          this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardSelectorMode = true;
+          availableCards.push({playerName : this.activePlayer, playerCards :  this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerOpenCards.filter(card => (card.color === availableColors))});
         }        
 
         break;
 
       case 'birthday':
+
         break;
 
       case 'unfair':
-        break;
+        let otherPlayers = this.allPlayerComponents.toArray().filter(elem => elem.playerName !== this.activePlayer);
+        for (const player of otherPlayers) {
+          let redCount = 0;
+          let yellowCount = 0;
+          let blueCount = 0;
+          let blackCount = 0;
+          let whiteCount = 0;
+          let cards : Card[] = [];
+          for (const card of player.playerOpenCards) {
+            switch (card.color){
+              case 'red':
+                redCount++;
+                break;
+              case 'yellow':
+                yellowCount++;
+                break;
+              case 'blue':
+                blueCount++;
+                break;
+              case 'black':
+                blackCount++;
+                break;
+              case 'white':
+                whiteCount++;
+                break;
+            }
+          }
 
-      case 'fair':
+          if (redCount % 3 !== 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'red')) {
+              cards.push(i);
+            } 
+          }
+          if (yellowCount % 3 !== 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'yellow')) {
+              cards.push(i);
+            } 
+          }
+          if (blueCount % 3 !== 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'blue')) {
+              cards.push(i);
+            } 
+          }
+          if (blackCount % 3 !== 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'black')) {
+              cards.push(i);
+            } 
+          }
+          if (whiteCount % 3 !== 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'white')) {
+              cards.push(i);
+            } 
+          }
+
+          availableCards.push({playerName : player.playerName, playerCards : cards});
+          this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardSelectorMode = true;
+
+        }
+
+
         break;
 
       case 'steal':
+
+        let _otherPlayers = this.allPlayerComponents.toArray().filter(elem => elem.playerName !== this.activePlayer);
+        for (const player of _otherPlayers) {
+          let redCount = 0;
+          let yellowCount = 0;
+          let blueCount = 0;
+          let blackCount = 0;
+          let whiteCount = 0;
+          let cards : Card[] = [];
+          for (const card of player.playerOpenCards) {
+            switch (card.color){
+              case 'red':
+                redCount++;
+                break;
+              case 'yellow':
+                yellowCount++;
+                break;
+              case 'blue':
+                blueCount++;
+                break;
+              case 'black':
+                blackCount++;
+                break;
+              case 'white':
+                whiteCount++;
+                break;
+            }
+          }
+
+          if (redCount % 3 === 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'red')) {
+              cards.push(i);
+            } 
+          }
+          if (yellowCount % 3 === 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'yellow')) {
+              cards.push(i);
+            } 
+          }
+          if (blueCount % 3 === 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'blue')) {
+              cards.push(i);
+            } 
+          }
+          if (blackCount % 3 === 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'black')) {
+              cards.push(i);
+            } 
+          }
+          if (whiteCount % 3 === 0){
+            for (const i of player.playerOpenCards.filter(elem => elem.color === 'white')) {
+              cards.push(i);
+            } 
+          }
+
+          availableCards.push({playerName : player.playerName, playerCards : cards});
+          this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardSelectorMode = true;
+
+        }
+        
         break;
+
+      case 'tax':
+        this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].opponentSelectorIsActive = true;
+        break;
+
+      case 'start':
+
+        if (this.cardDeck.length === 0){
+          return;
+        }
+
+        this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.splice(this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.indexOf(cardSelected),1);
+        this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardsPlayed++;
+        for (let i = 0; i < 2;) {
+          let selectedCardIndex = Math.floor(Math.random() * (this.cardDeck.length + 1));
+          console.log('selectedCardIndex',selectedCardIndex);
+          if (selectedCardIndex < this.cardDeck.length){
+            this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.push(this.cardDeck[selectedCardIndex]);
+            this.cardDeck.splice(selectedCardIndex, 1);
+            i++;
+          }    
+        }  
+        return;
     }
 
 
-    
-    // for (const player of this.allPlayerComponents.toArray()) {
-    //   if (player.playerName !== this.activePlayer){
-    //     availableCards.push({playerName : player.playerName, playerCards : player.playerOpenCards})
-    //   }
-    // }
     this.availableCardsToSelect = availableCards;
     console.log('availableCards',this.availableCardsToSelect);
      
+    
+  }
+
+  finalizeDeal(result : any){
+    console.log(result);
+    if (result.opponentName == ''){
+      let selectedOpponents = this.allPlayerComponents.toArray().filter(elem => elem.playerName !== this.activePlayer)[0];
+      console.log('select all opponents', selectedOpponents);
+      switch (result.action.action){
+        case 'birthday':
+          break;
+      }
+      
+    } else {
+      if (!this.checkJustNoCard(result.opponentName)){
+        let selectedOpponents = this.allPlayerComponents.toArray().filter(elem => elem.playerName == result.opponentName)[0];
+      
+        console.log('select one opponent', selectedOpponents);
+
+        switch (result.action.action){
+
+          case 'rent':
+            let propertyCounter = this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerOpenCards.filter(elem => elem.color == result.card.color).length;
+            if ((selectedOpponents.playerMoney - this.countRent(result.card.color, propertyCounter)) < 0){
+              this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerMoney += selectedOpponents.playerMoney;
+              selectedOpponents.playerMoney = 0;
+            } else {
+              selectedOpponents.playerMoney = selectedOpponents.playerMoney - this.countRent(result.card.color, propertyCounter);
+              this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerMoney += this.countRent(result.card.color, propertyCounter);
+            }
+            this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.splice(this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.indexOf(result.action),1);
+            this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardsPlayed++;
+            break;
+
+          case 'steal':
+            let temp = this.allPlayerComponents.toArray().filter(elem => (elem.playerName === result.opponentName))[0].playerOpenCards.filter(card => card.color === result.card.color);
+            for (let index = 0; index < 3; index++) {
+              this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerOpenCards.push(temp[index]);
+              temp.splice(0,1);
+            }
+            this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardsPlayed++;
+            break;
+
+          case 'unfair':
+            this.allPlayerComponents.toArray().filter(elem => (elem.playerName === result.opponentName))[0].playerOpenCards.splice(selectedOpponents.playerOpenCards.indexOf(result.card),1);
+            this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerOpenCards.push(result.card);
+            this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardsPlayed++;
+            break;
+
+          case 'tax':
+            if (this.allPlayerComponents.toArray().filter(elem => (elem.playerName === result.opponentName))[0].playerMoney <= 5){
+              this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerMoney += this.allPlayerComponents.toArray().filter(elem => (elem.playerName === result.opponentName))[0].playerMoney;
+              this.allPlayerComponents.toArray().filter(elem => (elem.playerName === result.opponentName))[0].playerMoney = 0;
+              
+            } else {
+              this.allPlayerComponents.toArray().filter(elem => (elem.playerName === result.opponentName))[0].playerMoney = this.allPlayerComponents.toArray().filter(elem => (elem.playerName === result.opponentName))[0].playerMoney - 5;
+              this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerMoney += 5;
+            }
+            this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.splice(this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.indexOf(result.action),1);
+            this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardsPlayed++;
+            break;
+      }
+      } else {
+        alert('Оппонент использовал карточку Просто скажи нет!');
+        this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.splice(this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].playerHand.indexOf(result.action),1);
+        this.allPlayerComponents.toArray().filter(elem => elem.playerName == this.activePlayer)[0].cardsPlayed++;
+      }
+    }
+  }
+
+  countRent(color : string, count : number){
+    switch (color){
+      case 'red':
+        switch (count){
+          case (1):
+            return 2;
+          case (2):
+            return 3;
+          case (3):
+            return 6;
+          default: 
+            return 6;
+        }
+      case 'yellow':
+        switch (count){
+          case (1):
+            return 2;
+          case (2):
+            return 3;
+          case (3):
+            return 6;
+          default: 
+            return 6;
+        }
+      case 'blue':
+        switch (count){
+          case (1):
+            return 4;
+          case (2):
+            return 8;
+          default: 
+            return 8;
+        }
+      case 'black':
+        switch (count){
+          case (1):
+            return 2;
+          case (2):
+            return 3;
+          case (3):
+            return 6;
+          default: 
+            return 6;
+        }
+      default:
+        switch (count){
+          case (1):
+            return 2;
+          case (2):
+            return 3;
+          case (3):
+            return 6;
+          default: 
+            return 6;
+        }
+    }
+  }
+
+  checkJustNoCard(opponentName : string){
+
+    if (this.allPlayerComponents.toArray().filter(elem => elem.playerName == opponentName)[0].playerHand.filter(card => card.action === 'justno').length != 0){
+      this.allPlayerComponents.toArray().filter(elem => elem.playerName == opponentName)[0].playerHand.splice(this.allPlayerComponents.toArray().filter(elem => elem.playerName == opponentName)[0].playerHand.indexOf(this.allPlayerComponents.toArray().filter(elem => elem.playerName == opponentName)[0].playerHand.filter(card => card.action === 'justno')[0]), 1);
+      return true;
+    } else {
+      return false;
+    }
     
   }
 
--- monopoly-web/src/app/player-field/player-field.component.ts
+++ monopoly-web/src/app/player-field/player-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, OnInit, OnChanges } from '@angular/core';
+
 import { Card } from '../card';
 
 @Component({
@@ -14,7 +15,15 @@ export class PlayerFieldComponent implements OnInit, OnChanges{
   playerOpenCards : Card[] = [];
   cardsPlayed : number = 0;
   cardSelectorMode : boolean = false;
+  opponentSelectorIsActive : boolean = false;
+  waitingCard! : Card;
+  selected! : {card : Card, opponent : string};
+  selectedOpp! : string;
+  _playedCard! : Card;
+  emptyCard : Card = new Card();
+  
 
+  @Input() opponentsList! : string[];
   @Input() isActivePlayer! : boolean;
   @Input() playerName! : string;
   @Input() recievedCards! : Card[];
@@ -22,25 +31,27 @@ export class PlayerFieldComponent implements OnInit, OnChanges{
   @Output() playedCard = new EventEmitter<Card>();
   @Output() passTurn = new EventEmitter();
   @Output() sendToDeck = new EventEmitter<Card>();
+  @Output() finalChackWithOpponents = new EventEmitter<{card : Card, opponentName : string, action : Card}>();
 
   ngOnInit(){
 
   }
 
   ngOnChanges(){
-    // console.log(this.recievedCards);
     
     for (const card of this.recievedCards) {
       this.playerHand.push(card);
     }
     
     this.recievedCards = [];
+
+    if (this.cardsPlayed == 3){
+      this.endTurn();
+    }
   }
   
 
   selectCard(selectedCard : Card){
-
-    
 
     let selectedCardIndex = this.playerHand.indexOf(selectedCard);
 
@@ -48,23 +59,27 @@ export class PlayerFieldComponent implements OnInit, OnChanges{
       case 'property':
         this.playerOpenCards.push(selectedCard);
         this.playerHand.splice(selectedCardIndex, 1);
+        this.cardsPlayed++;
         break;
       case 'money':
         this.playerMoney += selectedCard.price;
         this.sendToDeck.emit(selectedCard);
         this.playerHand.splice(selectedCardIndex, 1);
+        this.cardsPlayed++;
+        break;
+      case 'justno':
+        alert('Эта карта используется автоматически');
         break;
       default:
-
+        this._playedCard = selectedCard;
         this.playedCard.emit(selectedCard);
-        this.cardSelectorMode = true;
-
+        break;
     }
 
     
 
 
-    this.cardsPlayed++;
+   
     if (this.cardsPlayed == 3){
       this.endTurn();
     }
@@ -76,9 +91,29 @@ export class PlayerFieldComponent implements OnInit, OnChanges{
     this.passTurn.emit();
   }
 
-  checkDeal(){
-
+  checkDeal(){    
+    if (this._playedCard.action == 'rent' || this._playedCard.action == 'tax'){
+      this.opponentSelectorIsActive = true;
+      this.cardSelectorMode = false;
+      this.waitingCard = this.selected.card;
+    } else{
+      this.finalChackWithOpponents.emit({card : this.selected.card, opponentName : this.selected.opponent, action : this._playedCard});
+      this.cardSelectorMode = false;
+      this.selected = {card : this.emptyCard, opponent : ''};
+    }
   }
+
+  checkDealWithOpponent(){
+    this.finalChackWithOpponents.emit({card : this.waitingCard, opponentName : this.selectedOpp, action : this._playedCard});
+    this.opponentSelectorIsActive = false;
+    this.selected = {card : this.emptyCard, opponent : ''};
+    this.selectedOpp = '';
+  }
+
+  createTempObject(cardInput : Card, opponentInput : string){
+    return {card : cardInput, opponent : opponentInput}
+  }
+
 
 
 
